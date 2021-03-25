c9669d233364f587e7dfed54f3ebce6e9a4c4e26
Benjamin Miramontes <benjamin@miramontes.dev>
2021-03-25 18:01:04 -0400

    Created vs code workspace and added jsconfig for better jquery intellisense

--- src/www/isometric/budgets.js
+++ src/www/isometric/budgets.js
@@ -1,18 +1,26 @@
 import { apiRequest, login, register } from "./modules/api.js";
 import { errorToast } from "./modules/ui.js";
 
-$(async () => {
+async function updateBudgets() {
     const response = await apiRequest("/budget/list");
     if (response.error)
     {
-        errorToast("A network error has occured. Try refreshing the " +
-            "page when your internet connectivity returns.");
+        errorToast("A network error has occured. Reconnecting . . .");
         return;
     }
 
+    $("#budgets").html("");
     for (const budget of response.budgets)
     {
-        $("#budgets").appendChild($("<p>Test</p>"));
+        $("#budgets").appendChild($("<p class=\"budget\">Test</p>"));
         console.log(budget);
     }
+    if (response.budgets.length == 0)
+    {
+        $("#budgets").appendChild($("<p class=\"info\">No budgets found.</p>"));
+    }
+}
+
+$(async () => {
+    updateBudgets();
 });
