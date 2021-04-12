544625ac052ee0aaa1897181be9fb86a4451e3e0
Benjamin Miramontes <benjamin@miramontes.dev>
2021-04-12 13:13:45 -0400

    Added historical category value line graph

--- src/www/isometric/dashboard.js
+++ src/www/isometric/dashboard.js
@@ -15,7 +15,7 @@ function moneyLabel(label, value) {
 
 
 async function generatePieChart() {
-    // Generate pie chart data
+    // Generate chart data
     const categoryNames = [];
     const categoryAmounts = [];
     const categoryColors = [];
@@ -27,7 +27,7 @@ async function generatePieChart() {
         categoryColors.push(await deriveColor(category.name));
     }
 
-    // Render pie chart
+    // Render chart
     const pieChart = new Chart(
         document.getElementById('pieChart').getContext('2d'),
         {
@@ -45,6 +45,100 @@ async function generatePieChart() {
                     tooltip: {
                         callbacks: {
                             label: ctx => moneyLabel(ctx.label, ctx.parsed)
+                        }
+                    }
+                }
+            }
+        }
+    );
+}
+
+
+async function generateLineGraph() {
+    // Generate chart data
+    const categoryData = {};
+    const budgetLabels = [g_budget.name];
+    
+    const categories = await g_budget.categories();
+    for (const category of categories) {
+        categoryData[category.name] = {
+            values: [await category.total()],
+            color: await deriveColor(category.name)
+        }
+    }
+
+    // Add historical data
+    let parent = await g_budget.parent();
+    while (parent)
+    {
+        const missingCategories = new Set(Object.keys(categoryData));
+        const parentCategories = await parent.categories();
+        for (const category of parentCategories)
+        {
+            if (missingCategories.has(category.name))
+            {
+                missingCategories.delete(category.name);
+                categoryData[category.name].values.unshift(await category.total());
+            }
+        }
+        for (const name of missingCategories)
+        {
+            categoryData[name].values.unshift(0);
+        }
+        budgetLabels.unshift(parent.name);
+        parent = await parent.parent();
+    }
+
+    // Add future data
+    let child = await g_budget.child();
+    while (child)
+    {
+        const missingCategories = new Set(Object.keys(categoryData));
+        const childCategories = await child.categories();
+        for (const category of childCategories)
+        {
+            if (missingCategories.has(category.name))
+            {
+                missingCategories.delete(category.name);
+                categoryData[category.name].values.push(await category.total());
+            }
+        }
+        for (const name of missingCategories)
+        {
+            categoryData[name].values.push(0);
+        }
+        budgetLabels.push(child.name);
+        child = await child.child();
+    }
+
+    // Construct datasets
+    const datasets = [];
+    for (const [name, cdata] of Object.entries(categoryData))
+    {
+        datasets.push({
+            label: name,
+            data: cdata.values,
+            fill: false,
+            borderColor: cdata.color,
+            tension: 0.1
+        });
+    }
+
+    // Render chart
+    const lineGraph = new Chart(
+        document.getElementById('lineGraph').getContext('2d'),
+        {
+            type: 'line',
+            data: {
+                labels: budgetLabels,
+                datasets: datasets
+            },
+            options: {
+                maintainAspectRatio: false,
+                plugins: {
+                    tooltip: {
+                        callbacks: {
+                            label: ctx => moneyLabel(context.dataset.label || '', ctx.parsed.y)
                         }
                     }
                 }
@@ -77,4 +171,5 @@ $(async () => {
     });
 
     generatePieChart();
+    generateLineGraph();
 });
